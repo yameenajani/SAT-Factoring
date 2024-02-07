0d0f34bdb4f669d94e6196b026ec53db2cb3a0c9
Yameen Ajani <ajaniy@uwindsor.ca>
2024-02-07 12:32:36 -0800

    fix instance location dir

--- tests/scripts/get_instance.sh
+++ tests/scripts/get_instance.sh
@@ -8,12 +8,12 @@ do
 done
 
 echo "Generating instances for ${bitsize}"
-mkdir -p ../instances/$bitsize/new_instances
+mkdir -p ../instances/$bitsize/original_instances
 mkdir -p ../instances/$bitsize/cubes
 for ((i=1; i<=$num_instances; i++))
 do
-    python get_cnf.py ../data/$bitsize/data_$i.json ../instances/$bitsize/new_instances/instance_$i.cnf
-    python update_cnf.py ../data/$bitsize/data_$i.json ../instances/$bitsize/new_instances/instance_$i.cnf ../instances/$bitsize/cubes/cubes_$i.json
+    python get_cnf.py ../data/$bitsize/data_$i.json ../instances/$bitsize/original_instances/instance_$i.cnf
+    python update_cnf.py ../data/$bitsize/data_$i.json ../instances/$bitsize/original_instances/instance_$i.cnf ../instances/$bitsize/cubes/cubes_$i.json
 done
     # done
 # done
